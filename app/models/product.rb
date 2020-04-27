61da1533c9a9a20c19a59488046b2f0f9ed7ef9b
EC2 Default User <ec2-user@ip-172-31-41-153.ap-northeast-1.compute.internal>
2020-04-27 07:10:17 +0000

    -

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ validates :brand, presence: true
 validates :number, presence: true
 validates :name, presence: true
 validates :term, presence: true
-validates :name, uniqueness: true
 has_many :purchases, foreign_key: :product_number
 has_many :users, through: :purchases
 belongs_to :salon
